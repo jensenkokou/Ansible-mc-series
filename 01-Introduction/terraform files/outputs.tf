8821172b4b6725fcc0a94475df0e596893062c79
Jensen Kokou <104699414+jensenkokou@users.noreply.github.com>
2023-09-16 18:14:22 -0700

    Update outputs.tf

--- 01-Introduction/terraform files/outputs.tf
+++ 01-Introduction/terraform files/outputs.tf
@@ -1,27 +1,28 @@
 # Terraform Output Values
-output "ansible_publicIp" {
-  description = "EC2 Instance Public IP"
+output "Ansible-Public-Ip" {
+  description = "Ansible EC2 Instance Public IP"
   value       = aws_instance.ubuntu.public_ip
 }
 
-output "rhel_publicIp" {
-  description = "EC2 Instance Public IP"
+output "Rhel-Public-IP" {
+  description = "RHEL EC2 Instance Public IP"
   value       = aws_instance.rhel-hosts[*].public_ip
 }
 
-output "rhel_privateIp" {
-  description = "EC2 Instance Public IP"
+output "Rhel-Private-Ip" {
+  description = "RHEL EC2 Instance Private IP"
   value       = aws_instance.rhel-hosts[*].private_ip
 }
 
-# EC2 Instance Public IP
-output "instance_publicIp" {
-  description = "EC2 Instance Public IP"
+
+output "Instance_PublicIps" {
+  description = "Ubuntu EC2 Instance Public IP"
   value       = aws_instance.ubuntu-hosts[*].public_ip
 }
 
-# EC2 Instance Public DNS
-output "instance_privateIp" {
-  description = "EC2 Instance Private IP"
+output "Instance_PrivateIps" {
+  description = "Ubuntu EC2 Instance Private IP"
   value       = aws_instance.ubuntu-hosts[*].private_ip
 }
+
+
